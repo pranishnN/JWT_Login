d0d617924afd58aeb6dddcff8b457537aab5c7cb
Pranish N <pranishnallasivam512@hotmail.com>
2023-12-10 09:36:58 +0530

    model changes

--- Login/api/models.py
+++ Login/api/models.py
@@ -16,16 +16,16 @@ class Usermanager(BaseUserManager):
             user.save(using=self._db)
             return user
 
-    def create_superuser(self, emailId=None, password=None, **kwargs):
+    def create_superuser(self, email_id=None, password=None, **kwargs):
 
         user = self.create_user(
-            email_id=emailId,
+            email_id=email_id,
             password=password,
             isSuperAdmin=True,
-            is_otp_verified=True
+            is_otp_verified=True,
             isAdmin=True,
             **kwargs
-        )
+            )
 
         user.set_password(password)
         user.save(using=self._db)
@@ -35,10 +35,23 @@ class masUser(AbstractBaseUser, PermissionsMixin):
      
     first_name = models.CharField(null=True, max_length=100, blank=True)
     last_name = models.CharField(null=True, max_length=100, blank=True)
-    email_id = models.CharField(max_length=200, unique=True, error_messages={'unique':'This emailId already exists'})
+    email_id = models.CharField(max_length=200, unique=True, error_messages={'unique':'This email already exists'})
     otp = models.CharField(max_length=10, null=True, blank=True)
     is_otp_verified = models.BooleanField(default=False)
+    isSuperAdmin = models.BooleanField(default=False)
+    isAdmin = models.BooleanField(default=False)
     createdDate = models.DateTimeField(auto_now_add=True)
 
-    REQUIRED_FIELDS = ['password','emailId']
+    objects = Usermanager()
+
+    REQUIRED_FIELDS = ['password', 'first_name', 'last_name']
     USERNAME_FIELD = 'email_id'
+
+    def has_perm(self, perm, obj=None):
+        return self.isSuperAdmin
+
+    def has_module_perms(self, app_label):
+        return self.isSuperAdmin
+
+    def __str__(self):
+        return self.username
